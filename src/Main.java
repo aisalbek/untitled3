35af613b010eea75558a923f257f39cf7b87d2c4
aisalbek <110242974+aisalbek@users.noreply.github.com>
2022-10-05 11:31:31 +0600

    a

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+       Main main = new Main();
+       main.addTwoNumbers()
+
+    }
+        public LinkedList addTwoNumbers(LinkedList l1, LinkedList l2) {
+        
+        }
+            for (int i=0; i>l1.size();i++){
+
+            }
+    }
